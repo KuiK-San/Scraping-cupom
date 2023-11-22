65a5ff22446256e5ef7fd18b3c7118ad31dd2a27
Guilherme Casagrande <thegui4000@gmail.com>
2023-11-22 10:06:08 -0300

    feat: teste

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const video = document.getElementById('videoElement');
-    const cameraList = document.getElementById('cameraList');
-
-    // Verifica se o navegador suporta a API de mídia
-    if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
-        // Lista todas as dispositivos de mídia (câmeras, microfones, etc.)
-        navigator.mediaDevices.enumerateDevices()
-            .then(devices => {
-                devices.forEach(device => {
-                    // Verifica se o dispositivo é uma câmera
-                    if (device.kind === 'videoinput') {
-                        // Adiciona a câmera à lista
-                        const option = document.createElement('option');
-                        option.value = device.deviceId;
-                        option.text = device.label || `Câmera ${cameraList.options.length + 1}`;
-                        cameraList.appendChild(option);
-                    }
-                });
-            })
-            .catch(err => {
-                console.error('Erro ao listar dispositivos de mídia:', err);
-            });
-    } else {
-        console.error('A API de mídia não é suportada neste navegador.');
-    }
-
-    // Atualiza a fonte do vídeo quando o usuário seleciona uma câmera
-    cameraList.addEventListener('change', function() {
-        const selectedCameraId = cameraList.value;
-        const constraints = {
-            video: { deviceId: { exact: selectedCameraId } }
-        };
-
-        // Usa a API de mídia para acessar a câmera selecionada
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(stream => {
-                video.srcObject = stream;
-            })
-            .catch(err => {
-                console.error('Erro ao acessar a câmera:', err);
-            });
-    });
-});
+    const resultDiv = document.getElementById("result");
+    
     const video = document.getElementById("video");
 
     navigator.mediaDevices
